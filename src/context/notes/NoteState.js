621b15ce68d4851dd6a33482ddf417efa8b69b5a
unknown <you@example.com>
2024-06-06 20:26:52 +0530

    okokok

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,38 +2,89 @@ import React, { useState } from 'react'
 import noteContext from './noteContext'
 
 const NoteState = (props) => {
-    const notesInitial = [
-        {
-          "_id": "665f6dc3b196eec74d325eaf",
-          "user": "665f6d70b196eec74d325eab",
-          "title": "Lets Learn Python",
-          "description": "Python",
-          "tag": "default",
-          "date": "2024-06-04T19:40:51.545Z",
-          "__v": 0
-        },
-        {
-          "_id": "665f6dd0b196eec74d325eb1",
-          "user": "665f6d70b196eec74d325eab",
-          "title": "Lets Learn React",
-          "description": "React is nice",
-          "tag": "default",
-          "date": "2024-06-04T19:41:04.586Z",
-          "__v": 0
-        },
-        {
-          "_id": "665f6ddeb196eec74d325eb3",
-          "user": "665f6d70b196eec74d325eab",
-          "title": "Lets Learn Django",
-          "description": "React is Django",
-          "tag": "default",
-          "date": "2024-06-04T19:41:18.847Z",
-          "__v": 0
-        }
-      ]
-      const [notes, setnotes] = useState(notesInitial)
+  const host = "http://localhost:5000" 
+    const notesInitial = []
+      const [notes, setnotes] = useState(notesInitial);
+      const getAllNotes=async(id)=>{
+         //API CALL
+
+    const response = await fetch(`${host}/api/notes/fetchallnotes/${id}`, {
+      method: "GET",    
+         headers: {
+          "Content-Type": "application/json",
+        "auth-token": localStorage.getItem('token')
+         },  
+             });
+    const json = await response.json();   
+    console.log(json)
+
+
+
+      }
+      const addNote=async(title,description,tag)=>{
+ //API CALL
+
+    const response = await fetch(`${host}/api/notes/addnote`, {
+      method: "POST",    
+         headers: {
+          "Content-Type": "application/json",
+        "auth-token": localStorage.getItem('token')
+         },
+             body: JSON.stringify({title,description,tag}),  
+             });
+    const json = await response.json();   
+    console.log(json)
+
+             const note={
+              "_id": "665f6ddeb196eec74d325eb3",
+              "user": "665f6d70b196eec74d325eab",
+              "title": "Lets Learn Django",
+              "description": "React is Django",
+              "tag": "default",
+              "date": "2024-06-04T19:41:18.847Z",
+              "__v": 0
+             }
+            setnotes(notes.concat(note))
+
+      }
+      const editNote=async(id,title,description,tag)=>{
+ //API CALL
+
+    const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+      method: "PUT",    
+         headers: {
+          "Content-Type": "application/json",
+        "auth-token": localStorage.getItem('token')
+         },
+             body: JSON.stringify({title,description,tag}),  
+             });
+    const json = await response.json();   
+    console.log(json);
+
+
+
+
+
+      }
+      const deleteNote=async(id)=>{
+         //API CALL
+
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: "DELETE",    
+         headers: {
+          "Content-Type": "application/json",
+        "auth-token": localStorage.getItem('token')
+         },   
+             });
+    const json = await response.json();   
+    console.log(json)
+
+
+
+
+      }
   return (
-    <noteContext.Provider value={{notes,setnotes}}>
+    <noteContext.Provider value={{notes,setnotes,getAllNotes,addNote,editNote,deleteNote}}>
       {props.children}
     </noteContext.Provider>
   )
