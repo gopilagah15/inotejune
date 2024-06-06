2be13ae1a25d0501b4f6a1f456d587fed4ad289a
unknown <you@example.com>
2024-06-06 16:15:20 +0530

    last

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
 const mongoose = require('mongoose');
 PORT=5000;
@@ -18,6 +19,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use(cors())
 app.use(express.json());
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
