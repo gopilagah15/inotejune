48a21f1f09daea1c3fa1076b32826592078ee726
gopilagah15 <157889090+gopilagah15@users.noreply.github.com>
2024-08-16 18:44:22 +0530

    end

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home';
 import About from './Components/About';
-import Navbar from './Components/Navbar';
-import NoteState from './context/notes/NoteState';
+import Navbar from './Components/Navbar'; 
 
 function App() {
   return (
-    <>
-    <NoteState>
+    <> 
     <BrowserRouter>
     <Navbar/>
     <Routes>
       <Route path='/home' element={<Home/>}></Route>
       <Route path='/about' element={<About/>}></Route>
       </Routes>
-      </BrowserRouter>
-      </NoteState>
+      </BrowserRouter> 
     </>
   );
 }
